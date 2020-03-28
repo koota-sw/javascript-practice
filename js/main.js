1817c0c07e535f91220c7b457f6ec8352df316fc
Kota Miura <50856931+koota-sw@users.noreply.github.com>
2020-03-28 19:54:40 +0900

    少し整理

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function ()
 {
     $('#chap1').css('color', '#ebc000');
-    $('#chap1 .hello').css('transform', 'rotate(10deg)');
-    $('#chap1 .hello').css('opacity', 0.5);
+    $('#chap1 .hello').css({
+        transform: 'rotate(10deg)',
+        opacity: 0.5
+    })
 });
