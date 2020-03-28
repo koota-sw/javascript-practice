59c494cfa63bf3ad46579eff52aac18ad7939888
Kota Miura <50856931+koota-sw@users.noreply.github.com>
2020-03-28 20:01:39 +0900

    色変化
    文字の上にマウスが乗ると色が変わります。

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function ()
 {
-    $('#chap1').css('color', '#ebc000');
+    $('#chap1').css('color', '#ffffff');
+    $('#chap1').on('mouseover', function ()
+    {
+        $('#chap1').css('color', '#ebc000');
+    });
     $('#chap1 .hello').css({
         transform: 'rotate(10deg)',
         opacity: 0.5
