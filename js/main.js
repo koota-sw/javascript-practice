99a8e58ad406c26c4e3928c8344beb463699e3d3
Kota Miura <50856931+koota-sw@users.noreply.github.com>
2020-03-29 16:52:01 +0900

    少し整理
    コードの簡略化

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 $(function ()
 {
     $('#chap1').css('color', 'white');
-    $('.hello').on('mouseover', function ()
-    {
-        $('.hello').css('color', '#ebc000');
-        $('#chap1').css('background-color', '#ae5e9b');
-        $('header').css('color', '#ae5e9b');
-    });
-    $('.hello').on('mouseout', function ()
-    {
-        $('.hello').css('color', 'white');
-        $('#chap1').css('background-color', 'rgb(34, 105, 236)');
-        $('header').css('color', 'black');
-    });
-    $('#chap1 .hello').css({
-        transform: 'rotate(10deg)',
-        opacity: 0.5
-    })
+    $('.hello')
+        .on('mouseover', function ()
+        {
+            $('.hello').css('color', '#ebc000');
+            $('#chap1').css('background-color', '#ae5e9b');
+            $('header').css('color', '#ae5e9b');
+        })
+        .on('mouseout', function ()
+        {
+            $('.hello').css('color', 'white');
+            $('#chap1').css('background-color', 'rgb(34, 105, 236)');
+            $('header').css('color', 'black');
+        })
+        .css({
+            transform: 'rotate(10deg)',
+            opacity: 0.5
+        });
 });
